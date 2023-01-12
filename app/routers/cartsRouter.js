a853db2286af8b1a2eedba213e7ebfa30acfd040
Tiago Santa Maria <tiagosantamaria01@gmail.com>
2023-01-12 14:40:06 -0300

    Logica Terminada, Comienzo a pulir Detalle

--- app/routers/cartsRouter.js
+++ app/routers/cartsRouter.js
@@ -32,6 +32,21 @@ cartsRouter.post("/:cid/product/:pid", async(req,res)=>{
     res.send("HOLA")
 })
 
+//Endpoint para mostrar los productos del carrito
+cartsRouter.get("/:id", async (req,res)=>{
+    const {id} = req.params;
+    console.log(id);
+    const cart = await cartManager.getCartById(+id);
+    console.log(cart)
+    const productCart = [];
+    for(let i=0; i<cart.products.length;i++){
+        let product = await productManager.getProductById(cart.products[i].idProduct);
+        productCart.push(product)
+    }
+    console.log(productCart);
+    res.send(productCart)
+})
+
 //Exportar modulo
 module.exports = {
     cartsRouter,
